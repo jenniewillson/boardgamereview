e0ac5cd6ac5a9867c524b8ce2200b5b91ec148e3
Jennie Willson <ashfordct@btinternet.com>
2024-05-18 14:08:32 +0000

    FEAT: Basic setup of pages

--- run.py
+++ run.py
@@ -1,10 +1,26 @@
 import os
-from taskmanager import app
+from flask import Flask, render_template
+from taskmanager import app, db
 
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
 
 if __name__ == "__main__":
     app.run(
-        host=os.environ.get("IP"),
-        port=int(os.environ.get("PORT")),
-        debug=os.environ.get("DEBUG")
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
+        debug=os.environ.get("DEBUG") == "True"
     )
--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -6,11 +6,12 @@ if os.path.exists("env.py"):
 
 
 app = Flask(__name__)
+
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 
 uri = os.environ.get("DATABASE_URL")
 if uri.startswith("postgres://"):
   uri = uri.replace("postgres://", "postgresql://", 1)
 app.config["SQLALCHEMY_DATABASE_URI"] = uri
+
 db = SQLAlchemy(app)
-from taskmanager import routes # noqa
